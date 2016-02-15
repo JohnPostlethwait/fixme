049950e4c26e38d2d182fe10aefd742c8eb0f562
HansHammel <oh.schitt@freenet.de>
2016-02-15 16:22:06 +0100

    changed readme to reflect the changes

--- example/fixme.js
+++ example/fixme.js
@@ -5,5 +5,14 @@ fixme({
     ignored_directories: [],
     file_patterns: ['annotation_test.js'],
     file_encoding: 'utf8',
-    line_length_limit: 1000
+    line_length_limit: 40000,
+    color: true
+});
+fixme({
+    path: '../test/',
+    ignored_directories: [],
+    file_patterns: ['annotation_test.js'],
+    file_encoding: 'utf8',
+    line_length_limit: 40000,
+    color: false
 });
