69f3e9af1d03add7ec5ace106fe045aa53c98aee
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2017-02-26 17:58:13 -0500

    Making the annotation_test.js actually run a test and added the npm script for it

--- test/annotation_test.js
+++ test/annotation_test.js
@@ -5,3 +5,11 @@
 // XXX: This is the sample output for a XXX! XXX's need attention too!
 // FIXME (John Postlethwait): This is the sample output for a fixme! Seriously fix this...
 // BUG: This is the sample output for a bug! Who checked in a bug?!
+
+require('../bin/fixme')({
+  path:                 process.cwd(),
+  ignored_directories:  ['node_modules/**', '.git/**'],
+  file_patterns:        ['**/annotation_test.js'],
+  file_encoding:        'utf8',
+  line_length_limit:    1000
+});
