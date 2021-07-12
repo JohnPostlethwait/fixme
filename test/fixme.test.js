0fc50e8ea33b9800bde70cc64f2e9df9b22452c2
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-07-12 12:02:57 -0500

    Add mocha, chai, and sinon
    Call fixme but not much else

---
+++ test/fixme.test.js
@@ -0,0 +1,33 @@
+const chai = require("chai");
+const chalk = require("chalk");
+const sinon = require("sinon");
+const sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+const expect = chai.expect;
+
+const path = require("path");
+
+const fixme = require("../bin/fixme");
+
+chalk.level = 0;
+
+describe("fixme", () => {
+  beforeEach(() => {
+    sinon.spy(console, "log");
+  });
+
+  afterEach(() => {
+    console.log.restore();
+  });
+
+  it("should recognize a TODO", () => {
+    const spy = sinon.spy(console, "log");
+
+    const options = {
+      "path": path.join(__dirname, "cases", "valid"),
+      "file_patterns": [ "**/todo.txt" ],
+    };
+
+    fixme(options);
+  });
+});
