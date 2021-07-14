6b80b71427b5eed9b74abaeabbba6f42f6c40e12
Aiden Woodruff <aiden.woodruff@gmail.com>
2021-07-14 17:53:44 -0500

    Describe a valid annotation section
    Necesarry since invalid cases will also hook console.error

--- test/fixme.test.js
+++ test/fixme.test.js
@@ -12,161 +12,167 @@ const fixme = require('../bin/fixme');
 chalk.level = 0;
 
 describe('fixme', () => {
-  beforeEach(() => {
-    sinon.spy(console, 'log');
-  });
+  describe('valid annotations', () => {
+    const valid_cases = path.join(__dirname, 'cases', 'valid');
+    
+    beforeEach(() => {
+      sinon.spy(console, 'log');
+    });
 
-  afterEach(() => {
-    console.log.restore();
-  });
+    afterEach(() => {
+      console.log.restore();
+    });
 
-  const valid_cases = path.join(__dirname, 'cases', 'valid');
+    it('should recognize all verbs', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/verbs.txt' ],
+      };
 
-  it('should recognize all verbs', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/verbs.txt' ],
-    };
+      fixme(options, () => {
+        expect(console.log.callCount).to.equal(8);
 
-    fixme(options, () => {
-      expect(console.log.callCount).to.equal(8);
-
-      const messages = console.log.getCalls().map(c => c.args[0]);
+        const messages = console.log.getCalls().map(c => c.args[0]);
       
-      expect(messages[0]).to.equal('\n* verbs.txt [7 messages]:');
+        expect(messages[0]).to.equal('\n* verbs.txt [7 messages]:');
 
-      // TODO: Fix formatting here once it's changed in code.
-      expect(messages[1]).to.equal('  [Line  1]  ✐ NOTE: A note.');
-      expect(messages[2]).to.equal('  [Line  2]  ↻ OPTIMIZE: A possible optimization.');
-      expect(messages[3]).to.equal('  [Line  3]  ✓ TODO: A todo.');
-      expect(messages[4]).to.equal('  [Line  4]  ✄ HACK: A hack.');
-      expect(messages[5]).to.equal('  [Line  5]  ✗ XXX: An alternate fixme style.');
-      expect(messages[6]).to.equal('  [Line  6]  ☠ FIXME: A fixme.');
-      expect(messages[7]).to.equal('  [Line  7]  ☢ BUG: A bug.');
+        // TODO: Fix formatting here once it's changed in code.
+        expect(messages[1]).to.equal('  [Line  1]  ✐ NOTE: A note.');
+        expect(messages[2]).to.equal('  [Line  2]  ↻ OPTIMIZE: A possible optimization.');
+        expect(messages[3]).to.equal('  [Line  3]  ✓ TODO: A todo.');
+        expect(messages[4]).to.equal('  [Line  4]  ✄ HACK: A hack.');
+        expect(messages[5]).to.equal('  [Line  5]  ✗ XXX: An alternate fixme style.');
+        expect(messages[6]).to.equal('  [Line  6]  ☠ FIXME: A fixme.');
+        expect(messages[7]).to.equal('  [Line  7]  ☢ BUG: A bug.');
 
-      done();
+        done();
+      });
     });
-  });
 
-  it('should recognize strip whitespace', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/whitespace.txt' ],
-    };
+    it('should recognize strip whitespace', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/whitespace.txt' ],
+      };
 
-    fixme(options, () => {
-      expect(console.log.secondCall.args[0]).to.equal(
-        '  [Line 1]  ↻ OPTIMIZE: Maybe limit use of whitespace?'
-      );
+      fixme(options, () => {
+        expect(console.log.secondCall.args[0]).to.equal(
+          '  [Line 1]  ↻ OPTIMIZE: Maybe limit use of whitespace?'
+        );
       
-      done();        
+        done();        
+      });
     });
-  });
 
-  it('should recognize comments after code', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/after-code.txt' ],
-    };
+    it('should recognize comments after code', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/after-code.txt' ],
+      };
 
-    fixme(options, () => {
-      expect(console.log.secondCall.args[0]).to.equal(
-        '  [Line 1]  ✐ NOTE: That\'s some code.'
-      );
+      fixme(options, () => {
+        expect(console.log.secondCall.args[0]).to.equal(
+          '  [Line 1]  ✐ NOTE: That\'s some code.'
+        );
 
-      done();
+        done();
+      });
     });
-  });
 
-  it('should recognize an author', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/author.txt' ],
-    };
+    it('should recognize an author', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/author.txt' ],
+      };
 
-    fixme(options, () => {
-      expect(console.log.secondCall.args[0]).to.equal(
-        '  [Line 1]  ✓ TODO from Jimmbo: Have a party.'
-      );
-      expect(console.log.thirdCall.args[0]).to.equal(
-        '  [Line 5]  ✓ TODO from Mary: Grow.'
-      );
+      fixme(options, () => {
+        expect(console.log.secondCall.args[0]).to.equal(
+          '  [Line 1]  ✓ TODO from Jimmbo: Have a party.'
+        );
+        expect(console.log.thirdCall.args[0]).to.equal(
+          '  [Line 5]  ✓ TODO from Mary: Grow.'
+        );
 
-      done();
+        done();
+      });
     });
-  });
 
-  it('should recognize single character messages', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/one-character.txt']
-    };
+    it('should recognize single character messages', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/one-character.txt']
+      };
 
-    fixme(options, () => {
-      const messages = console.log.getCalls().map(c => c.args[0]);
+      fixme(options, () => {
+        const messages = console.log.getCalls().map(c => c.args[0]);
 
-      expect(messages[1]).to.equal('  [Line 1]  ✄ HACK: .');
-      expect(messages[2]).to.equal('  [Line 4]  ✄ HACK: .');
-      expect(messages[3]).to.equal('  [Line 5]  ✄ HACK: .');
-      expect(messages[4]).to.equal('  [Line 6]  ✄ HACK: .');
+        expect(messages[1]).to.equal('  [Line 1]  ✄ HACK: .');
+        expect(messages[2]).to.equal('  [Line 4]  ✄ HACK: .');
+        expect(messages[3]).to.equal('  [Line 5]  ✄ HACK: .');
+        expect(messages[4]).to.equal('  [Line 6]  ✄ HACK: .');
 
-      done();
+        done();
+      });
     });
-  });
 
-  it('should recognize empty messages', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/no-character.txt']
-    };
+    it('should recognize empty messages', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/no-character.txt']
+      };
 
-    fixme(options, () => {
-      const messages = console.log.getCalls().map(c => c.args[0]);
+      fixme(options, () => {
+        const messages = console.log.getCalls().map(c => c.args[0]);
 
-      expect(messages[1]).to.equal('  [Line 1]  ✗ XXX: [[no message to display]]');
-      expect(messages[2]).to.equal('  [Line 4]  ↻ OPTIMIZE: [[no message to display]]');
-      expect(messages[3]).to.equal('  [Line 5]  ↻ OPTIMIZE: [[no message to display]]');
-      expect(messages[4]).to.equal('  [Line 6]  ↻ OPTIMIZE: [[no message to display]]');
+        expect(messages[1]).to.equal('  [Line 1]  ✗ XXX: [[no message to display]]');
+        expect(messages[2]).to.equal('  [Line 4]  ↻ OPTIMIZE: [[no message to display]]');
+        expect(messages[3]).to.equal('  [Line 5]  ↻ OPTIMIZE: [[no message to display]]');
+        expect(messages[4]).to.equal('  [Line 6]  ↻ OPTIMIZE: [[no message to display]]');
 
-      done();
+        done();
+      });
     });
-  });
 
-  it('should recognize multiline comments', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/multiline-note.txt']
-    };
+    it('should recognize multiline comments', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/multiline-note.txt']
+      };
 
-    fixme(options, () => {
-      const messages = console.log.getCalls().map(c => c.args[0]);
+      fixme(options, () => {
+        const messages = console.log.getCalls().map(c => c.args[0]);
 
-      expect(messages[1]).to.equal(
-        '  [Line  1]  ✐ NOTE: A note\nwhose content\nspans multiple\nlines.'
-      );
-      expect(messages[2]).to.equal(
-        '  [Line  6]  ✐ NOTE: XML note spanning\nmultiple lines.'
-      );
-      expect(messages[3]).to.equal('  [Line 10]  ✐ NOTE: Multiple\nlines.');
+        expect(messages[1]).to.equal(
+          '  [Line  1]  ✐ NOTE: A note\nwhose content\nspans multiple\nlines.'
+        );
+        expect(messages[2]).to.equal(
+          '  [Line  6]  ✐ NOTE: XML note spanning\nmultiple lines.'
+        );
+        expect(messages[3]).to.equal('  [Line 10]  ✐ NOTE: Multiple\nlines.');
 
-      done();
+        done();
+      });
     });
-  });
   
-  it('should strip only leading and trailing whitespace from multiline messages', (done) => {
-    const options = {
-      'path': valid_cases,
-      'file_patterns': [ '**/whitespace-multiline.txt']
-    };
+    it('should strip only leading and trailing whitespace from multiline messages', (done) => {
+      const options = {
+        'path': valid_cases,
+        'file_patterns': [ '**/whitespace-multiline.txt']
+      };
 
-    fixme(options, () => {
-      const messages = console.log.getCalls().map(c => c.args[0]);
+      fixme(options, () => {
+        const messages = console.log.getCalls().map(c => c.args[0]);
 
-      expect(messages[1]).to.equal(
-        '  [Line  1]  ✐ NOTE: The leading whitespace will be stripped\n\nBut not the stuff above here.'
-      );
+        expect(messages[1]).to.equal(
+          '  [Line  1]  ✐ NOTE: The leading whitespace will be stripped\n\nBut not the stuff above here.'
+        );
 
-      done();
+        done();
+      });
     });
+  });
+
+  describe('invalid annotations', () => {
+    const invalid_cases = path.join(__dirname, 'cases', 'invalid');
   });
 });
